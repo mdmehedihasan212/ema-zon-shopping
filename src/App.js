5dafdfd549179d431b9301dbc0630a8568d5b096
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-04-11 03:46:33 +0600

    add home component with design

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 import About from './Components/About/About';
 import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
@@ -8,6 +8,7 @@ import Orders from './Components/Orders/Orders';
 import Product from './Components/Product/Product';
 import Shipping from './Components/Shipping/Shipping';
 import Shop from './Components/Shop/Shop';
+import LogIn from './Components/LogIn/LogIn';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Route path='/shipping' element={<Shipping></Shipping>}></Route>
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/product' element={<Product></Product>}></Route>
+        <Route path='/login' element={<LogIn></LogIn>}></Route>
         <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import './Home.css';
+import photo from '../../Assets/photo-1552573102-2b44b44d85b5.avif';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home Page</h1>
-        </div>
+        <section className='home-container'>
+            <div className="text-section">
+                <h4>New Collection For Fall</h4>
+                <p>Discover all the new arrivals of ready-to-wear collection.</p>
+                <Link to={'/shop'} className='shop-btn'>Shop Now</Link>
+            </div>
+            <div className="photo-section">
+                <img src={photo} alt="" />
+            </div>
+        </section>
     );
 };
 
