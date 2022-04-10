499439b98d94740046f78d4ecddf5db43400a4a1
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-04-10 22:18:36 +0600

    add cart design and home component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
 import Product from './Components/Product/Product';
 import Shop from './Components/Shop/Shop';
 
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Header></Header>
       <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
         <Route path='/shop' element={<Shop></Shop>}></Route>
         <Route path='/product' element={<Product></Product>}></Route>
       </Routes>
--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -7,6 +7,15 @@ const Product = ({ product }) => {
     return (
         <section className='product-container'>
             <img src={img} alt="" />
+            <div className="product-details">
+                <h4 title={name}>
+                    {name.length > 25 ? name.slice(0, 25) + '...' : name}
+                </h4>
+                <h5>Price: $ {price}</h5>
+                <p>Manufacturer : {seller}</p>
+                <p>Rating : {ratings} star</p>
+            </div>
+            <button className='product-add-btn'>Add to Cart</button>
         </section>
     );
 };
